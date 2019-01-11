ff35e543b1cfcbdcf6ad4cb226ed4e7f2815dd0e
Piotr Kozakowski <kozak000@gmail.com>
2019-01-11 16:43:51 +0100

    Add an argument for SSH port

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Usage: ./t2t.sh image_name
 
 
-nvidia-docker run -d -v $PWD/tensor2tensor:/t2t -v $PWD/data:/data -p 2222:22 -it $1
+nvidia-docker run -d -v $PWD/tensor2tensor:/t2t -v $PWD/data:/data -p $2:22 -it $1
